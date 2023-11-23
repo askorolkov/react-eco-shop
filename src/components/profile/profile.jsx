e5801f46d0d6d4556ffd75a58c79c9233e1d3f01
Aleksey Korolkov <askorolkov1@gmail.com>
2023-11-23 14:36:48 +0500

    добавляет список понравившихся предметов в профиль

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,11 +1,24 @@
 import './profile.css';
 
-function Profile() {
+function Profile(props) {
   return(
     <div className="profile">
-      <h2 className="profile__title">Profile</h2>
-      <p className="profile__info">Name: Vasya</p>
-      <p className="profile__info">Email: vasyapup@haha.ru</p>
+      <h2 className="profile__title">Профиль</h2>
+
+      <p className="profile__info">Имя: Вася</p>
+      <p className="profile__info">Email: vasyapup@vasya.ru</p>
+
+      <div className="profile__likes">
+        <h3 className="profile__likes-title">Список понравившихся предметов</h3> 
+          {props.likedItems.map((item, i)=> (
+            <div className="profile__liked-item" key={i}>
+              <img src={item.src}  alt="" className='profile__liked-pic'/>
+              <h3 className="profile__liked-name">{item.name}</h3>
+              <p className="profile__liked-price">{item.price} &#8381;</p>
+            </div>           
+            )
+          )}      
+      </div>
     </div>
   )
 }
